53808e69491e1d669e9f37ff26058042d375f880
asif <asifshekh0597@gmail.com>
2022-10-19 16:45:24 +0530

    first commit

---
+++ PracticeForGit/src/main/java/com/marlabs/PracticeForGitApplication.java
@@ -0,0 +1,13 @@
+package com.marlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeForGitApplication.class, args);
+	}
+
+}
